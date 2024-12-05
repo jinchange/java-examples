03a78b715620ea3c014751406e24ffb0a3ad7811
Zhang Jin <zhangjin@algorix.co>
2024-12-05 10:33:07 +0800

    init

---
+++ src/main/java/com/jinchanc/javaexamples/JavaExamplesApplication.java
@@ -0,0 +1,13 @@
+package com.jinchanc.javaexamples;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaExamplesApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JavaExamplesApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/jinchanc/javaexamples/JavaExamplesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jinchanc.javaexamples;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaExamplesApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
